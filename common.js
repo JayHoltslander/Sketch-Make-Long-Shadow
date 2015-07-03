14b1ea558ef352fe4b395e1e20ece58feb3067c2
Aby N <aby@scooterinteractive.com>
2015-07-03 11:22:14 +0800

    UndoManager fix
    - Fixed an issue where Undo Manager would go into an invalid state
    after running the plugin, because of too many nested undo groups.

--- common.js
+++ common.js
@@ -51,6 +51,15 @@ var createDropDown = function(items, selectedItemIndex, width) {
 	return dropdownButton;
 }
 
+var setUndoGroupingEnabled = function(enabled) {
+	var undoManager = [[doc currentView] undoManager];
+    if(enabled) {
+        [undoManager enableUndoCoalescing];
+    } else {
+        [undoManager disableUndoCoalescing];
+    }
+}
+
 //--------------------------------------
 //  Shapes and Styles
 //--------------------------------------
