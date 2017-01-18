5d565479d7aab64e49605b3affc380b9c24247ce
Aby Nimbalkar <aby@scooterinteractive.com>
2017-01-18 12:53:54 +0800

    Legacy fix
    - Repackaged the plugin so it doesn’t show up in the Legacy submenu
    - Added and icon and description for Sketch Runner integration

--- Make Long Shadow.sketchplugin/Contents/Sketch/script.js
+++ Make Long Shadow.sketchplugin/Contents/Sketch/script.js
@@ -1,31 +1,25 @@
-@import 'common.js'
+var kPluginDomain = "com.holtslander.jay.sketch-make-long-shadow"
+var kPluginName = "Make Long Shadow";
 
-kPluginName = "Make Long Shadow"
-
-// Create presets so we can remember the user's 
-// selection next time the plugin is run
-
-var presets = {
-	shadowDistance: 80,
-	shadowDirection: "Bottom Right"
-}
-var userDefaults = initDefaults(presets)
-
-// All possible directions for the shadow
-
-var possibleDirections = [ "Bottom Right", "Bottom Left", "Top Right", "Top Left" ]
-
-
-function run() {
-	
+var onRun = function(context) {
 	// Check selection
+	var selection = context.selection;
 	if ([selection count] == 0) {
 		showDialog("Please select something")
 		return
 	}
 
-	// Ask for shadow details
+	// Create presets so we can remember the user's 
+	// selection next time the plugin is run
+	userDefaults = initDefaults({
+		shadowDistance: 80,
+		shadowDirection: "Bottom Right"
+	})
+
+	// All possible directions for the shadow
+	var possibleDirections = [ "Bottom Right", "Bottom Left", "Top Right", "Top Left" ]
 	
+	// Ask for shadow details
 	var alert = [COSAlertWindow new];
 	[alert setMessageText: kPluginName];
 
@@ -45,6 +39,9 @@ function run() {
 	var directionDropdown = createDropDown(possibleDirections, indexOfLastSelectedDirection, 140)
 	[alert addAccessoryView: directionDropdown] // 3
 
+	var distanceField = [alert viewAtIndex: 1];
+	alert.alert().window().setInitialFirstResponder(distanceField);
+
 	// Show config window and handle user's resposnse
 	
 	var response = [alert runModal]
@@ -55,7 +52,8 @@ function run() {
 
 		// Iterate through selected layers
 		
-		var loop = [selection objectEnumerator],
+		var doc = context.document,
+			loop = [selection objectEnumerator],
 			distance = parseInt([[alert viewAtIndex: 1] stringValue]),
 			direction = [directionDropdown titleOfSelectedItem],
 			xMultiplier = (direction == "Bottom Left" || direction == "Top Left") ? -1 : 1,
@@ -187,8 +185,123 @@ function run() {
 		saveDefaults(userDefaults)
 
 	}
-
 }
 
 
-run()
+//--------------------------------------
+//  Persisting information
+//--------------------------------------
+
+var initDefaults = function(initialValues) {
+
+	var defaults = [[NSUserDefaults standardUserDefaults] objectForKey:kPluginDomain]
+	var defaultValues = {}
+    var dVal;
+
+    for (var key in defaults) {
+    	defaultValues[key] = defaults[key]
+	}
+
+	for (var key in initialValues) {
+		dVal = defaultValues[key]
+		if (dVal == nil) defaultValues[key] = initialValues[key]
+	}
+
+	return defaultValues
+}
+
+var saveDefaults = function(newValues) {
+	if (kPluginDomain) {
+		var defaults = [NSUserDefaults standardUserDefaults]
+		[defaults setObject: newValues forKey: kPluginDomain];
+	}
+}
+
+//--------------------------------------
+//  Plugin UI
+//--------------------------------------
+
+var showDialog = function(message, title) {
+	var app = [NSApplication sharedApplication],
+		title = (typeof title !== 'undefined') ? title : kPluginName;
+    [app displayDialog:message withTitle:title]
+}
+
+var createDropDown = function(items, selectedItemIndex, width) {
+	var width = width || 300,
+		selectedItemIndex = selectedItemIndex || 0,
+		dropdownButton = [[NSPopUpButton alloc] initWithFrame: NSMakeRect(0, 0, width, 25) pullsDown:false];
+	[dropdownButton addItemsWithTitles: items];
+	[dropdownButton selectItemAtIndex: selectedItemIndex];
+	return dropdownButton;
+}
+
+//--------------------------------------
+//  Shapes and Styles
+//--------------------------------------
+
+var hexToMSColor = function(hex, alpha) {
+	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex),
+		red = parseInt(result[1], 16) / 255,
+		green = parseInt(result[2], 16) / 255,
+		blue = parseInt(result[3], 16) / 255,
+		alpha = (typeof alpha !== 'undefined') ? alpha : 1,
+		color = [NSColor colorWithCalibratedRed:red green:green blue:blue alpha:alpha];
+	return [MSColor colorWithNSColor:color]
+}
+
+var clearLayerStyles = function(layer) {
+	var style = [layer style]
+	[[style fills] removeAllObjects]
+	[[style shadows] removeAllObjects]
+	[[style innerShadows] removeAllObjects]
+	[[style borders] removeAllObjects]
+	
+	[[style blur] setIsEnabled:false]
+	[style setTextStyle:nil]
+}
+
+
+var cleanShapePath = function(shapePathLayer, threshold) {
+
+	if(!shapePathLayer || [shapePathLayer class] !== MSShapePathLayer) return
+
+	var shapeFrame = [shapePathLayer frame],
+		shapeWidth = [shapeFrame width],
+		shapeHeight = [shapeFrame height],
+		shapePath = [shapePathLayer path],
+		prevPoint = [shapePath firstPoint],
+		point = [prevPoint point],
+		prevCGPoint = CGPointMake(point.x*shapeWidth, point.y*shapeHeight),
+		newPoints = [],
+		count = [shapePath numberOfPoints],
+		excludePrev = false,
+		i, currPoint, currCGPoint;
+
+	for(i = 1; i < count; ++i) {
+
+		currPoint = [shapePath pointAtIndex:i]
+		point = [currPoint point]
+		currCGPoint = CGPointMake(point.x*shapeWidth, point.y*shapeHeight)
+		
+		if(Math.abs(prevCGPoint.x-currCGPoint.x) <= threshold && Math.abs(prevCGPoint.y-currCGPoint.y) <= threshold) {
+			if(!excludePrev) {
+				newPoints.push(prevPoint)
+				excludePrev = true
+			}
+		} else {
+			newPoints.push(prevPoint)
+			if(excludePrev) excludePrev = false
+		}
+		
+		prevPoint = currPoint
+		prevCGPoint = currCGPoint
+	}
+
+	newPoints.push([shapePath lastPoint])
+
+	var newShapePath = [MSShapePath pathWithPoints:newPoints]
+	[newShapePath setIsClosed:true]
+	shapePathLayer.path = newShapePath
+
+}
